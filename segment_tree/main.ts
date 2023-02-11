e6e476a2d9a81a880fe15fba649eee4438017886
Yukkku <yukku.scratcher@gmail.com>
2023-02-11 18:58:05 +0900

    Segment Treeを多少まともに修正。TypeScriptむずすぎ。

--- segment_tree/main.ts
+++ segment_tree/main.ts
@@ -1,79 +1,135 @@
 "use strict";
 
-type group<T> = {
-  e: T,
-  op: (a: T, b: T) => T,
+interface SegTreeConstructor<T> extends ArrayConstructor {
+  new (...i: T[] | [number]): SegTree<T>;
+  prototype: SegTree<T>;
+  op: (a: T, b: T) => T;
+  e: T;
+}
+
+interface SegTree<T> extends Array<T> {
+  constructor: SegTreeConstructor<T>;
+  roots?: SegTree<T>;
+  prod (a: number, b: number): T
 }
 
 /**
- * SegmentTreeの実装。ArrayとProxyを用いて可変長なものになっている。
- * constructor以外の新しいSegmentTreeを返すメソッドを使うとバグるので注意。
+ * セグ木のクラスを作る関数です。
+ * @param op モノイドの演算
+ * @param e モノイドの単位元
  */
-export class SegmentTree<T> extends Array<T> {
-  /** モノイドの演算 */
-  public op: (a: T, b: T) => T;
-  /** モノイドの単位元 */
-  public e: T;
-  /** 半分の長さを持つこの配列の根のセグ木 */
-  private roots?: SegmentTree<T>;
+const SegTree = <T>(op: (a: T, b: T) => T, e: T): SegTreeConstructor<T> => {
+  const SegTree = (class extends Array<T> {
+    static op = op;
+    static e = e;
 
-  constructor (op: (a: T, b: T) => T, e: T) {
-    super();
-    this.op = op;
-    this.e = e;
+    roots?: SegTree<T>;
+  
+    constructor () {
+      super(...arguments);
 
-    return new Proxy(this, {
-      set (target, p, newValue) {
-        target[p as keyof SegmentTree<T>] = newValue;
-  
-        if (typeof p === "string" && !Number.isNaN(Number(p)) && target.length > 1) {
-          if (target.roots === undefined) {
-            target.roots = new SegmentTree<T>(op, e);
-          }
-  
-          const index = Number(p);
-          const rootIndex = index >> 1;
-  
-          if ((index & 1) === 1 || index + 1 !== target.length) {
-            target.roots[rootIndex] = op(target[rootIndex << 1], target[(rootIndex << 1) + 1]);
-          }
-        } else if (p === "length") {
-          if (target.roots !== undefined) {
-            target.roots.length = (newValue as number) >> 1;
-          }
+      if (this.length >= 2) {
+        this.roots = new SegTree();
+
+        for (let i = 1; i < this.length; i += 2) {
+          this.roots.push(op(this[i - 1], this[i]));
         }
+      }
+
+      return new Proxy(this, {
+        get (target, p) {
+          if (typeof p === 'string' && !Number.isNaN(Number(p))) {
+            if (!Object.hasOwn(target, p)) {
+              return e;
+            }
+          }
   
-        return true;
-      },
-    });
-  }
+          return target[p as keyof SegTree<T>];
+        },
+  
+        set (target, p, val) {
+          target[p as keyof SegTree<T>] = val;
 
-  /**
-   * 区間[bigin, end)にopを適用させた結果を返す。
-   * @param bigin 区間の始め(含む)
-   * @param end 区間の終わり(含まない)
-   */
-  prod(bigin: number, end: number): T  {
-    if (bigin >= end) {
-      return this.e;
+          if (typeof p === 'string' && !Number.isNaN(Number(p))) {
+            const index = Number(p);
+        
+            if (target.length > 1) {
+              if (target.roots === undefined) {
+                target.roots = new SegTree();
+              }
+        
+              const rootIndex = index >> 1;
+        
+              if ((index & 1) === 1 || index + 1 !== target.length) {
+                target.roots[rootIndex] = SegTree.op(target[rootIndex << 1], target[(rootIndex << 1) + 1]);
+              }
+            }
+          } else {
+            if (p === 'length') {
+              if (target.roots !== undefined) {
+                target.roots.length = Number(val) >> 1;
+              }
+            }
+          }
+  
+          return true;
+        },
+      });
     }
+  
+    prod(bigin: number, end: number) {
+      if (bigin < 0) {
+        bigin = 0;
+      }
 
-    let retVal = this.e;
+      if (end > this.length) {
+        end = this.length;
+      }
 
-    if (bigin & 1) {
-      retVal = this[bigin];
-      bigin += 1;
+      let retVal = SegTree.e;
+  
+      if (bigin >= end) {
+        return retVal;
+      }
+  
+      if (bigin & 1) {
+        retVal = this[bigin];
+        bigin += 1;
+      }
+  
+      if (this.roots !== undefined) {
+        retVal = SegTree.op(retVal, this.roots.prod(bigin >> 1, end >> 1));
+      }
+  
+      if (end & 1) {
+        retVal = SegTree.op(retVal, this[end - 1]);
+      }
+  
+      return retVal;
     }
+  }) as unknown as SegTreeConstructor<T>;
 
-    if (this.roots !== undefined) {
-      retVal = this.op(retVal, this.roots.prod(bigin >> 1, end >> 1));
-    }
-
-    if (end & 1) {
-      end -= 1;
-      retVal = this.op(retVal, this[end]);
-    }
-
-    return retVal;
-  }
+  return SegTree;
 }
+
+SegTree.sum = SegTree<number>(
+  (a: number, b: number) => a + b,
+  0,
+);
+
+SegTree.min = SegTree<number>(
+  Math.min,
+  Infinity,
+);
+
+SegTree.max = SegTree<number>(
+  Math.max,
+  -Infinity,
+);
+
+SegTree.xor = SegTree<number>(
+  (a: number, b: number) => a ^ b,
+  0,
+);
+
+export { SegTree };
